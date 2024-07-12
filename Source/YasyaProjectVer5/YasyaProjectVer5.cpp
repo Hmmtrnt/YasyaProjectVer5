e7a79cd1f4c063fb66d72ae9c0747988330a8927
Hmmtrnt <114457374+Hmmtrnt@users.noreply.github.com>
2024-07-12 11:27:30 +0900

    Initial commit

---
+++ Source/YasyaProjectVer5/YasyaProjectVer5.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "YasyaProjectVer5.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, YasyaProjectVer5, "YasyaProjectVer5" );
---
+++ Source/YasyaProjectVer5/YasyaProjectVer5GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "YasyaProjectVer5GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YASYAPROJECTVER5_API AYasyaProjectVer5GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
